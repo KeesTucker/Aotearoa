12e5fc2cec34ef7d361eff13134cb935860cb57e
Kees <me@keestucker.com>
2024-09-30 19:08:22 +1300

    fix: got it to compile!!

--- Source/AotearoaEditor.Target.cs
+++ Source/AotearoaEditor.Target.cs
@@ -9,7 +9,7 @@ public class AotearoaEditorTarget : TargetRules
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
 		ExtraModuleNames.Add("Aotearoa");
 	}
 }
